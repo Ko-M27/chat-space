ea3ceb59432a89a221a62719141356ec96b40baf
Ko-M27 <m.kodai9527@gmail.com>
2020-06-04 09:56:55 +0900

    user-edit-edit1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def edit
+
+  end
+
+  def update
+    if current_user.params(user_params)
+      redirct_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
