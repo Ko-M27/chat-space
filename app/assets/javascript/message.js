37ae334ff1eb99858c3c3066568a5d81101e27ec
Ko-M27 <m.kodai9527@gmail.com>
2020-06-11 12:46:45 +0900

    Ajax_message

---
+++ app/assets/javascript/message.js
@@ -0,0 +1,69 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ){
+      let html =
+        `<div class="message-container">
+          <div class="message-info">
+            <p class="message-info__name">
+              ${message.user}
+            </p>
+            <p class="message-info__date">
+              ${message.created_at}
+            </p>
+          </div>
+          <div class="message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+            <img class="message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message-container">
+        <div class="message-info">
+          <p class="message-info__name">
+            ${message.user}
+          </p>
+          <p class="message-info__date">
+           ${message.created_at}
+         </p>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    }
+  };
+  $('.message-form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST', 
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.message-list').append(html);
+      $('.message-form')[0].reset();
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      $('.input-form').val('')
+      $('.send-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert('メッセージを入力してください。');
+      $('.message-form')[0].reset();
+      $('.input-form').val('')
+      $('.send-btn').prop('disabled', false);
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.content @message.content
+json.image @message.image.url
+json.user @message.user.name
+json.group @message.group.name
+json.created_at @message.created_at.strftime("%Y年%m月%d日 %H:%M")
